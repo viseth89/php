cc2db8a4adb158ea29446cb95dbfd1f9bbec7938
viseth89 <viseth89@gmail.com>
2019-03-28 08:47:05 -0500

    added post form and redirects

--- shareposts/app/controllers/Posts.php
+++ shareposts/app/controllers/Posts.php
@@ -11,11 +11,23 @@
       // Get posts
 
       $posts = $this->postModel->getPosts();
+      
       $data = [
         'posts' => $posts
       ];
 
       // Load view
       $this->view('posts/index', $data);
+    }
+
+
+    public function add() {
+      $data = [
+        'title' =>  '',
+        'body'  =>  '',
+      ];
+
+      // Load view
+      $this->view('posts/add', $data);
     }
   }
