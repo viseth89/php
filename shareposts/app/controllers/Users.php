6976b3b70e1c4bd326be73ed1fc4f34f6d486843
viseth89 <viseth89@gmail.com>
2019-03-27 23:36:21 -0500

    login and logout done, working on posts

--- shareposts/app/controllers/Users.php
+++ shareposts/app/controllers/Users.php
@@ -143,7 +143,7 @@ class Users extends Controller
 
         if($loggedInUser){
           // Create session
-          die('SUCCESS');
+          $this->createUserSession($loggedInUser);
         } else {
           $data['password_err'] = 'Password incorrect';
 
@@ -167,6 +167,30 @@ class Users extends Controller
 
         // Load view
         $this->view('users/login', $data);
+      }
+    }
+
+    public function createUserSession($user){
+      $_SESSION['user_id'] = $user->id;
+      $_SESSION['user_email'] = $user->email;
+      $_SESSION['user_name'] = $user->name;
+      redirect('pages/index');
+    }
+
+    // Unset session = logout
+    public function logout() {
+      unset($_SESSION['user_id']);
+      unset($_SESSION['user_email']);
+      unset($_SESSION['user_name']);
+      session_destroy();
+      redirect('users/login');
+    }
+
+    public function isLoggedIn(){
+      if(isset($_SESSION['user_id'])){
+        return true;
+      } else {
+        return false;
       }
     }
   }
--- shareposts/app/views/inc/navbar.php
+++ shareposts/app/views/inc/navbar.php
@@ -1,28 +1,37 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-3">
-<div class="container">
-<a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+    <div class="container">
+        <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo URLROOT; ?>">Home </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
-            </li>
-        </ul>
-<!-- Float second ul to the right -->
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Register </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
-            </li>
-        </ul>
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>">Home </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
+                </li>
+            </ul>
+            <!-- Float second ul to the right -->
+            <ul class="navbar-nav ml-auto">
+
+            <!-- If logged in/out display particular items -->
+                <?php if (isset($_SESSION['user_id'])) : ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout </a>
+                </li>
+
+                <?php else : ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Register </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
+                </li>
+                <?php endif; ?>
+            </ul>
+        </div>
     </div>
-</div>
 </nav> 
